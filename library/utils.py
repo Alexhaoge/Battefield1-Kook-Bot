60240e5c372d32baab2457ba664862dd10317074
Zhihao Lyu <zlyu@mail.nankai.edu.cn>
2023-06-07 05:05:35 -0700

    upd: add findserver, improve stat

--- library/utils.py
+++ library/utils.py
@@ -1,13 +1,14 @@
 import json
 import requests
+from typing import Union
 
 API_SITE = "https://api.gametools.network/"
 
-def request_API(game, prop='stats', params={}):
+def request_API(game, prop='stats', params={}) -> Union[dict, requests.Response]:
     url = API_SITE+f'{game}/{prop}'
 
     res = requests.get(url,params=params)
     if res.status_code == 200:
         return json.loads(res.text)
     else:
-        raise requests.HTTPError
+        return res
--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import logging
+import requests
 
 from khl import Bot, Message, MessageTypes
 from secret import token
 from random import choice
 
 from library.utils import request_API
-from library.cardTemplate import render_stat_card
+from library.cardTemplate import (
+    render_stat_card, render_find_server_card
+)
 
 bot = Bot(token=token)
 
@@ -24,15 +27,33 @@ async def bf1_player_stat(msg: Message, origin_id: str):
     /stat originid
     """
     result = request_API('bf1','all',{'name':origin_id, 'lang':'zh-tw'})
-    # result['__update_time'] = time()
-    # html = apply_template(result,'bf1', '/')
-    # pic = await html_to_pic(html, viewport={"width": 700,"height":10})
-    cm = render_stat_card(result)
-    await msg.reply(cm)
+    if isinstance(result, requests.Response):
+        if result.status_code == 404:
+            logging.warning(f'BF1 User {origin_id} not found')
+        await msg.reply(f'玩家{origin_id}不存在')
+        return
+    await msg.reply(render_stat_card(result))
 
 @bot.command(name='f')
 async def bf1_find_server(msg: Message, server_name: str):
+    result = request_API('bf1', 'servers', {'name': server_name, 'lang':'zh-tw'})
+    if isinstance(result, requests.Response):
+        if result.status_code == 404:
+            logging.warning(f'error getting server info')
+        await msg.reply(f'找不到任何相关服务器')
+        return
+    await msg.reply(render_find_server_card(result))
+
+@bot.command(name='r')
+async def bf1_recent(msg: Message, origin_id: str):
+    result = request_API('bf1','all',{'name':origin_id, 'lang':'zh-tw'})
+    if isinstance(result, requests.Response):
+        if result.status_code == 404:
+            logging.warning(f'BF1 User {origin_id} not found')
+        await msg.reply(f'玩家{origin_id}不存在')
+        return
     pass
+    #await msg.reply(cm)
 
 logging.basicConfig(level='INFO')
 bot.run()
